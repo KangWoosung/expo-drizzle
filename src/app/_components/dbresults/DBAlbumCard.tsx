2034c2802a284a040b5720fc150663b61848844b
KangWoosung <wkang386@gmail.com>
2025-01-07 23:44:38 +0900

    After move folders

--- src/app/_components/dbresults/DBAlbumCard.tsx
+++ src/app/_components/dbresults/DBAlbumCard.tsx
@@ -11,51 +11,53 @@ interface DBAlbumCardProps {
 }
 
 export default function DBAlbumCard({ album }: DBAlbumCardProps) {
-  const { artistObj: artist } = useArtistAlbumZustand();
+  const { artistZustandObj: artistObj, albumZustandObj: albumObj } =
+    useArtistAlbumZustand();
 
   return (
     <Card className="w-full max-w-md mx-auto my-4 ">
       <CardHeader className="pb-2 bg-slate-50">
         <View className="flex flex-row justify-between">
           <CardTitle className="text-2xl font-extrabold">
-            {artist.name}
+            {artistObj.name}
           </CardTitle>
-          {artist.score !== undefined && (
+          {artistObj.score !== undefined && (
             <View className="flex items-center space-x-1">
               <Text className="text-sm font-medium">Score:</Text>
-              <Text className="text-lg font-bold">{artist.score}</Text>
+              <Text className="text-lg font-bold">{artistObj.score}</Text>
               <Text className="text-xs text-muted-foreground">/100</Text>
             </View>
           )}
         </View>
-        {artist.sort_name && artist.sort_name !== artist.name && (
+        {artistObj.sort_name && artistObj.sort_name !== artistObj.name && (
           <Text className="text-sm text-muted-foreground">
-            {artist.sort_name}
+            {artistObj.sort_name}
           </Text>
         )}
       </CardHeader>
       <CardContent className="grid gap-4 pt-4">
-        {artist.type && (
+        {artistObj.type && (
           <View className="flex flex-row items-center gap-2">
             <Ionicons name="people-outline" size={18} color={"#64748b"} />
-            <Text className="text-lg text-slate-500">{artist.type}</Text>
+            <Text className="text-lg text-slate-500">{artistObj.type}</Text>
           </View>
         )}
-        {artist.country && (
+        {artistObj.country && (
           <View className="flex flex-row items-center gap-2">
             <Ionicons name="flag-outline" size={18} color={"#64748b"} />
-            <Text className="text-lg text-slate-500">{artist.country}</Text>
+            <Text className="text-lg text-slate-500">{artistObj.country}</Text>
           </View>
         )}
-        {(artist.begin_date || artist.end_date) && (
+        {(artistObj.begin_date || artistObj.end_date) && (
           <View className="flex items-left space-x-2">
             {/* <CalendarIcon className="h-4 w-4 text-muted-foreground" /> */}
             <Text className="text-sm">
-              {artist.begin_date || "Unknown"} - {artist.end_date || "Present"}
+              {artistObj.begin_date || "Unknown"} -{" "}
+              {artistObj.end_date || "Present"}
             </Text>
           </View>
         )}
-        {artist.albumsCnt ? (
+        {artistObj.albumsCnt ? (
           <View className="flex flex-row justify-between">
             <View className="flex flex-row items-center gap-2">
               <Ionicons
@@ -63,7 +65,7 @@ export default function DBAlbumCard({ album }: DBAlbumCardProps) {
                 size={18}
                 color={"#64748b"}
               />
-              <Text className="text-sm">Albums: {artist.albumsCnt}</Text>
+              <Text className="text-sm">Albums: {artistObj.albumsCnt}</Text>
             </View>
             <Text className="text-sm">To Albums</Text>
           </View>
@@ -76,10 +78,13 @@ export default function DBAlbumCard({ album }: DBAlbumCardProps) {
                 color={"#64748b"}
               />
               <Text className="text-lg text-slate-500">
-                Albums: {artist.albumsCnt ? artist.albumsCnt : 0}
+                Albums: {artistObj.albumsCnt ? artistObj.albumsCnt : 0}
               </Text>
             </View>
-            <Link className="mx-6" href={`../../artists/detail/${artist.id}`}>
+            <Link
+              className="mx-6"
+              href={`../../artistjs/detail/${artistObj.id}`}
+            >
               <Ionicons
                 name="arrow-down-circle-outline"
                 size={24}
@@ -88,7 +93,7 @@ export default function DBAlbumCard({ album }: DBAlbumCardProps) {
             </Link>
           </View>
         )}
-        {artist.disambiguation && (
+        {artistObj.disambiguation && (
           <View className="flex flex-row items-center gap-2">
             <Ionicons
               name="information-circle-outline"
@@ -96,14 +101,14 @@ export default function DBAlbumCard({ album }: DBAlbumCardProps) {
               color={"#295491"}
             />
             <Text className="text-base text-muted-foreground">
-              {artist.disambiguation}
+              {artistObj.disambiguation}
             </Text>
           </View>
         )}
         {/* {tags && tags.length > 0 && <Tags tags={tags} />} */}
         <View className="flex flex-row justify-between items-left">
           <Badge variant="secondary" className="w-fit">
-            <Text>ID: {artist.id}</Text>
+            <Text>ID: {artistObj.id}</Text>
           </Badge>
           <Pressable className="px-3 py-2 border rounded-md" onPress={() => {}}>
             <Ionicons name="trash-outline" size={20} color={"#295491"} />
